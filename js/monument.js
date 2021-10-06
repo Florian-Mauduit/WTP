d02534db7f32a841e80c8e28925147bdab210950
FlorianMauduit <flomauduit@orange.fr>
2021-10-06 09:55:23 +0200

    Réglage des bugs du popup et mise en place du texte

--- js/monument.js
+++ js/monument.js
@@ -3,63 +3,53 @@
 let paris = document.getElementById('monumentTourEiffel');
 let parisOverlay = document.getElementById('overlayTourEiffel');
 let btnCloseTour = document.getElementById('btnCloseTourEiffel');
-paris.addEventListener('click',openMoadl);
-btnCloseTour.addEventListener('click',closeModal);
+paris.addEventListener('click',() => openMoadl(parisOverlay));
+btnCloseTour.addEventListener('click',() => closeModal(parisOverlay));
 // Popup Arc de Triomphes
 
 let arcDeTriomphes = document.getElementById('monumentArcDeTriomphes');
 let arcDeTriomphesOverlay = document.getElementById('overlayArcDeTriomphes');
 let btnCloseArc = document.getElementById('btnCloseArcDeTriomphes');
-arcDeTriomphes.addEventListener('click', openMoadl);
-btnCloseArc.addEventListener('click',closeModal);
+arcDeTriomphes.addEventListener('click',() => openMoadl(arcDeTriomphesOverlay));
+btnCloseArc.addEventListener('click',() => closeModal(arcDeTriomphesOverlay));
 
 // Popup Cathédrale Notre Dame
 
 let cathedraleNotreDame = document.getElementById('monumentCathedraleNotreDame');
 let cathedraleNotreDameOverlay = document.getElementById('overlayCathedraleNotreDame');
 let btnCloseNotreDame = document.getElementById('btnCloseCathedraleNotreDame');
-cathedraleNotreDame.addEventListener('click', openMoadl);
-btnCloseNotreDame.addEventListener('click',closeModal);
+cathedraleNotreDame.addEventListener('click',() => openMoadl(cathedraleNotreDameOverlay));
+btnCloseNotreDame.addEventListener('click',() => closeModal(cathedraleNotreDameOverlay));
 
 //  Popup Le Louvres
 let leLouvres = document.getElementById('monumentleLouvres');
 let leLouvresOverlay = document.getElementById('overlayLouvresMonument');
 let btnCloseleLouvres = document.getElementById('btnCloseLouvresMonument');
-leLouvres.addEventListener('click', openMoadl);
-btnCloseleLouvres.addEventListener('click',closeModal);
+leLouvres.addEventListener('click',() => openMoadl(leLouvresOverlay));
+btnCloseleLouvres.addEventListener('click',() => closeModal(leLouvresOverlay));
 
 // Popup Panthéon
 let pantheon = document.getElementById('monumentPantheon');
 let pantheonOverlay = document.getElementById('overlayPantheon');
 let btnClosePantheonMonument = document.getElementById('btnClosePantheon');
-pantheon.addEventListener('click', openMoadl);
-btnClosePantheonMonument.addEventListener('click',closeModal);
+pantheon.addEventListener('click', () => openMoadl(pantheonOverlay));
+btnClosePantheonMonument.addEventListener('click',() => closeModal(pantheonOverlay));
 
 // Popup Place de la Concorde
 let placeDeLaConcorde = document.getElementById('monumentLaConcorde');
 let placeDeLaConcordeOverlay = document.getElementById('overlayLaConcorde');
 let btnClosePlace = document.getElementById('btnCloseLaConcorde');
-placeDeLaConcorde.addEventListener('click', openMoadl);
-btnClosePlace.addEventListener('click',closeModal);
+placeDeLaConcorde.addEventListener('click',() => openMoadl(placeDeLaConcordeOverlay));
+btnClosePlace.addEventListener('click',() => closeModal(placeDeLaConcordeOverlay));
 
 // function ouverture de la Popup
 
-function openMoadl() {
-    parisOverlay.style.display = 'block';
-    arcDeTriomphesOverlay.style.display = 'block';
-    cathedraleNotreDameOverlay.style.display = 'block';
-    leLouvresOverlay.style.display = 'block';
-    pantheonOverlay.style.display = 'block';
-    placeDeLaConcordeOverlay.style.display = 'block';
+function openMoadl(monument) {
+    monument.style.display = 'block';
 }
 
 // function fermeture de la popup
 
-function closeModal(){
-    parisOverlay.style.display = 'none';
-    arcDeTriomphesOverlay.style.display = 'none';
-    cathedraleNotreDameOverlay.style.display = 'none';
-    leLouvresOverlay.style.display = 'none';
-    pantheonOverlay.style.display = 'none';
-    placeDeLaConcordeOverlay.style.display = 'none';
+function closeModal(monument){
+    monument.style.display =  'none';
 }
